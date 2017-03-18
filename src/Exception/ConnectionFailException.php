64096c19a432122c7d990de12fc3f825247ee2fe
Philipp Lang <philipp@aweos.de>
2017-03-18 19:09:52 +0000

    Initial Commit

---
+++ src/Exception/ConnectionFailException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Zoomyboy\PhpSsh\Exception;
+
+class ConnectionFailException extends \Exception {}
---
+++ src/Ssh.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zoomyboy\PhpSsh;
+
+use \phpseclib\Net\SSH2;
+use \phpseclib\Crypt\RSA;
+use Zoomyboy\PhpSsh\Exception\ConnectionFailException;
+
+class Ssh {
+
+	private $connection = false;
+
+	public static function connect($host, $user, $privateKeyFile) {
+		$ssh = new self();
+		$ssh->connection = new SSH2($host);
+		$key = new RSA();
+		$key->loadKey(file_get_contents($privateKeyFile));
+		if (!$ssh->connection->login($user, $key)) {
+			throw new ConnectionFailException('Connection to '.$host.' failed');
+		}
+
+		return $ssh;
+	}
+
+	public function __call($method, $params) {
+		return $this->connection->{$method}($params[0]);
+	}
+}
