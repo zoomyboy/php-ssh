0885c54ce954209e467b3e16af254c4b223d3fb0
Philipp Lang <pille@zoomyboy.de>
2017-03-19 01:22:20 +0100

    Fixed: Failure on no host

--- src/Ssh.php
+++ src/Ssh.php
@@ -11,22 +11,32 @@ class Ssh {
 	private $connection = false;
 
 	public static function connect($host, $user, $privateKeyFile) {
-		$ssh = new self();
-		$ssh->connection = new SSH2($host);
-		$key = new RSA();
-		$key->loadKey(file_get_contents($privateKeyFile));
-		if (!$ssh->connection->login($user, $key)) {
-			throw new ConnectionFailException('Connection to '.$host.' failed');
-		}
+		try {
+			$ssh = new self();
+			$ssh->connection = new SSH2($host);
+			$key = new RSA();
+			$key->loadKey(file_get_contents($privateKeyFile));
+			if (!$ssh->connection->login($user, $key)) {
+				throw new ConnectionFailException('Connection to '.$host.' failed', 1);
+			} 
 
-		return $ssh;
+			return $ssh;
+		} catch(\ErrorException $e) {
+			throw new ConnectionFailException('Host not found!', 2);
+		}
 	}
 
 	public function __call($method, $params) {
 		return $this->connection->{$method}($params[0]);
 	}
 
-	public static function getKeyFiles($path) {
+	public static function getPublicKeyFiles($path) {
 		return glob($path.'/*.pub');
+	}
+
+	public static function getPrivateKeyFiles($path) {
+		return array_filter(glob($path.'/*'), function($file) {
+			return !preg_match('/\.pub$/', $file);
+		});
 	}
 }
