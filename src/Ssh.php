81f36f4fbef3021584756bfd5504dbefcd9ce2d0
Philipp Lang <philipp@aweos.de>
2017-03-28 14:57:00 +0000

    Add ssh fluid app

--- src/Ssh.php
+++ src/Ssh.php
@@ -9,18 +9,43 @@ use Zoomyboy\PhpSsh\Exceptions\ConnectionFailException;
 class Ssh {
 
 	private $connection = false;
+	private $auth;
 
-	public static function connect($host, $user, $privateKeyFile) {
+	public static function auth($host, $user) {
+		$ssh = new self();
+		$ssh->connection = new SSH2($host);
+		$ssh->host = $host;
+		$ssh->user = $user;
+
+		return $ssh;
+	}
+
+	public function check() {
 		try {
-			$ssh = new self();
-			$ssh->connection = new SSH2($host);
-			$key = new RSA();
-			$key->loadKey(file_get_contents($privateKeyFile));
-			if (!$ssh->connection->login($user, $key)) {
-				throw new ConnectionFailException('Connection to '.$host.' failed', 1);
-			} 
+			return $this->connection->login($this->user, $this->auth) == true;
+		} catch(\ErrorException $e) {
+			return false;
+		}
+	}
 
-			return $ssh;
+	public function withKeyFile($keyFile) {
+		$key = new RSA();
+		$key->loadKey(file_get_contents($keyFile));
+		$this->auth = $key;
+
+		return $this;
+	}
+
+	public function withPassword($password) {
+		$this->auth = $password;
+
+		return $this;
+	}
+
+	public function connect() {
+		try {
+			$this->connection->login($this->user, $password);
+			return $this;
 		} catch(\ErrorException $e) {
 			throw new ConnectionFailException('Host not found!', 2);
 		}
